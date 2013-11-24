9f986a990567c2cc1767c26535652dd509941476
julcar91 <juliac91@gmail.com>
2013-11-24 14:43:41 -0500

    updated nominees.py pretty much done

--- nominees.py
+++ nominees.py
@@ -9,27 +9,31 @@ import unicodedata
 
 import re
 
-def fixWeirdChars(stringy):
-	matchObj = re.search(r'&#x..', stringy)
+#get rid of accents etc. 
+def fixWeirdChars(phrase):
+	matchObj = re.search(r'&#x..', phrase)
 	if matchObj:
    		accentedHex = matchObj.group()
    		HexPart = accentedHex.split('x')[1]
-   		Hexy = "0x" + HexPart
-   		Num = int(Hexy, 16)
-   		Char = chr(Num)
-   		stringy = re.sub((accentedHex+';'), Char, stringy)
-   		#print stringy
-
-   		matchAgain = re.search(r'&#x..', stringy)
-   		if(matchAgain):
-   			return fixWeirdChars(stringy)
+   		if(HexPart == 'E9'):
+   			phrase = re.sub((accentedHex+';'), 'e', phrase)
    		else:
-   			return stringy
+	   		Hexy = "0x" + HexPart
+	   		Num = int(Hexy, 16)
+	   		Char = chr(Num)
+	   		phrase = re.sub((accentedHex+';'), Char, phrase)
+
+   		matchAgain = re.search(r'&#x..', phrase)
+   		if(matchAgain):
+   			return fixWeirdChars(phrase)
+   		else:
+   			return phrase
    	else:
-   		return stringy
+   		return phrase
 
 
 def clean_unicode(s):
+	s = str(s)
 	s.encode('ascii','ignore')
 	s = fixWeirdChars(s)
  	return str(s)
@@ -45,22 +49,17 @@ Outputs a dict of the format:
 '''
 def get_by_year(year):
 
-	output = open("all_nominees.csv", "wb")
-	writer = csv.writer(output)
-
-	#if(year == 2012):
 	url = URL("http://www.imdb.com/event/ev0000003/" + str(year))
 	dom = DOM(url.download(cached=True))
 	
 	dictAll = {}
-	#for val in range (0,5):
+	
 	awards = dom.by_class('award')
 	awardTitles = awards[0].by_tag('h2')
 	awardList = []
 	for award in awardTitles:
-		awardList.append(clean_unicode(award.content))
-	 	#dictAll[award.content]
-	#print awardList
+		awardList.append(award.content)
+
 	prize = awards[0].by_tag('blockquote')
 	for index, title in enumerate(prize[1:25]):
 		winner = title.by_tag('strong')[0].by_tag('a')[0].content
@@ -68,12 +67,12 @@ def get_by_year(year):
 		nomineeList = []
 	 	for each in title.by_tag('strong')[1::]:
 	 		nomineeList.append(clean_unicode(each.by_tag('a')[0].content))
-	 	
+	 		
 	 	winnersAndNominees = {}
 	  	winnersAndNominees['winner'] = clean_unicode(winner)
 	  	winnersAndNominees['nominees'] = nomineeList
 	 	dictAll[awardList[index]] =  winnersAndNominees
-	print dictAll['Best Performance by an Actor in a Leading Role']	
+	#print dictAll['Best Performance by an Actor in a Leading Role']	
 	return dictAll
 
 '''
@@ -87,45 +86,101 @@ Outputs a dict of dicts the format:
 '''
 def get_all_years():
 	hugeDict = {}  #lol
+	#for year in range(1935, 2014):
 	for year in range(1935, 2014):
 		hugeDict[year] = get_by_year(str(year))
 	return hugeDict #LOLLLL
 
 
-def get_by_award_all_years(oscar):
-	yearDict = {}
-	for year in range(1950, 2014):
-		url = URL("http://www.imdb.com/event/ev0000003/" + str(year))
-		dom = DOM(url.download(cached=True))
-		
-		awards = dom.by_class('award')
-		awardTitles = awards[0].by_tag('h2')
-		index = 1
-		isin = False
-		for award in awardTitles:
-			if (oscar == clean_unicode(award.content)):
-				isin = True
-				break
-			elif (index > 30):
-				break
-			else:
-				index += 1
-		
-		if(isin):
-			prize = awards[0].by_tag('blockquote')
-			winnersAndNominees = {}
-			
-			winner = prize[index].by_tag('strong')[0].by_tag('a')[0].content
-			#print winner
-			nomineeList = []
-			for each in prize[index].by_tag('strong')[1::]:
-			 	nomineeList.append(clean_unicode(each.by_tag('a')[0].content))
-			  	winnersAndNominees['winner'] = clean_unicode(winner)
-			  	winnersAndNominees['nominees'] = nomineeList
+#a little hard coding, unfortunately 
+def repair_dict(officialOscars, dictionary):
+	for year in dictionary:
+		for award in dictionary[year]:
+			if(award == 'Best Motion Picture of the Year'):
+				dictionary[year]['Best Picture'] = dictionary[year]['Best Motion Picture of the Year']
+				del dictionary[year]['Best Motion Picture of the Year']
 
-			yearDict[year] =  winnersAndNominees
-	print yearDict
+			if(award == 'Best Performance by an Actor in a Leading Role'):
+				dictionary[year]['Best Actor in a Leading Role'] = dictionary[year]['Best Performance by an Actor in a Leading Role']
+				del dictionary[year]['Best Performance by an Actor in a Leading Role']
+
+			if(award == 'Best Performance by an Actress in a Leading Role'):
+				dictionary[year]['Best Actress in a Leading Role'] = dictionary[year]['Best Performance by an Actress in a Leading Role']
+				del dictionary[year]['Best Performance by an Actress in a Leading Role']
+
+			if(award == 'Best Performance by an Actress in a Supporting Role'):
+				dictionary[year]['Best Actress in a Supporting Role'] = dictionary[year]['Best Performance by an Actress in a Supporting Role']
+				del dictionary[year]['Best Performance by an Actress in a Supporting Role']
+
+			if(award == 'Best Performance by an Actor in a Supporting Role'):
+				dictionary[year]['Best Actor in a Supporting Role'] = dictionary[year]['Best Performance by an Actor in a Supporting Role']
+				del dictionary[year]['Best Performance by an Actor in a Supporting Role']
+
+			if(award == 'Best Writing, Original Screenplay'):
+				dictionary[year]['Best Writing, Screenplay Written Directly for the Screen'] = dictionary[year]['Best Writing, Original Screenplay']
+				del dictionary[year]['Best Writing, Original Screenplay']
+
+			if(award == 'Best Writing, Adapted Screenplay'):
+				dictionary[year]['Best Writing, Screenplay Based on Material from Another Medium'] = dictionary[year]['Best Writing, Adapted Screenplay']
+				del dictionary[year]['Best Writing, Adapted Screenplay']
+
+			if(award == 'Best Foreign Language Film of the Year'):
+				newAwardName = award
+				newAwardName = re.sub(' of the Year', '', newAwardName)
+				dictionary[year][newAwardName] = dictionary[year][award]
+				del dictionary[year][award]
+
+			#check to see if it includes best achievement
+			if('Best Achievement' in award):
+				if(award == 'Best Achievement in Directing'):
+					dictionary[year]['Best Director'] = dictionary[year]['Best Achievement in Directing']
+					del dictionary[year]['Best Achievement in Directing']
+				else:
+					newAwardName = award
+					if('Mixing' in award):
+						newAwardName = re.sub(' Mixing', '', newAwardName)
+
+					if(' and Hairstyling' in award):
+						newAwardName = re.sub(' and Hairstyling', '', newAwardName)
+
+					if('Music Written for Motion Pictures, Original Song' or 'Music Written for Motion Pictures, Original Score' in award):
+						newAwardName = re.sub(' Written for Motion Pictures', '', newAwardName)
+
+
+					newAwardName = re.sub(' Achievement in', '', newAwardName)
+
+					if 'Visual Effects' in newAwardName:
+						#add comma
+						newAwardName = 'Best Effects, Visual Effects' 
+
+					dictionary[year][newAwardName] = dictionary[year][award]
+					del dictionary[year][award]
+					
+	return dictionary
+
+
+def get_by_award_all_years(oscar, dictionary):
+	yearDict = {}
+	for year in dictionary:
+		if oscar in dictionary[year].keys():
+			yearDict[year] = dictionary[year][oscar]
 	return yearDict
 
-#get_by_year(2013)
-get_by_award_all_years('Best Performance by an Actress in a Leading Role')
+
+#EXAMPLE OF USAGE:
+allDataEver = get_all_years()
+allDataEver = repair_dict(oscarArray, allDataEver)
+print allDataEver[2013]['Best Cinematography']
+print get_by_award_all_years('Best Picture', allDataEver) 
+print get_by_year(1952)
+
+#official oscar titles
+#note Best Picture instead of Best Achievement in Picture etc. 
+#note that not all oscars exist for every year, i.e. best black and white 
+oscarArray = ['Best Picture', 'Best Actor in a Leading Role', 'Best Actress in a Leading Role', 'Best Actor in a Supporting Role', 'Best Actress in a Supporting Role', 'Best Actor in a Supporting Role', 'Best Director', 'Best Assistant Director', 'Best Writing, Screenplay Written Directly for the Screen', 'Best Writing, Screenplay Based on Material from Another Medium', 'Best Cinematography', 'Best Art Direction-Set Decoration', 'Best Costume Design', 'Best Sound', 'Best Film Editing', 'Best Sound Editing','Best Effects, Visual Effects', 'Best Makeup', 'Best Music, Original Song', 'Best Music, Original Score', 'Best Short Film, Animated', 'Best Short Film, Live Action', 'Best Documentary, Short Subjects', 'Best Documentary, Features', 'Best Foreign Language Film']
+
+
+
+
+
+
